d43cfcd955c96bcc9d5658281a19d10c262bf3de
Luizsellitto <luizsellitto2609@gmail.com>
2024-09-09 13:56:24 -0400

    Endereco
    Classe-Endereco

---
+++ Product-Rent/Models/Endereco.cs
@@ -0,0 +1,12 @@
+﻿namespace Product_Rent.Models
+{
+    public class Endereco
+    {
+        public string CEP { get; set; }
+        public string Estado { get; set; }
+        public string Cidade { get; set; }
+        public string Rua { get; set; }
+        public string Bairro { get; set; }
+        public int Numero { get; set; }
+    }
+}
