d46c8fbab156caeaa8398d0a33e045849d4b44b3
Thiciane Frata <100950692+Thiciane@users.noreply.github.com>
2024-10-22 23:58:15 -0300

    ✨ feat: Função que persiste um novo funcionário no banco e retorna o id dele.

--- Product-Rent/Models/FuncionarioDAO.cs
+++ Product-Rent/Models/FuncionarioDAO.cs
@@ -12,5 +12,43 @@ namespace Product_Rent.Models
         {
             conn = new ConnectionMysql();
         }
+
+        public int Insert(Funcionario item)
+        {
+            try
+            {
+                var query = conn.Query();
+                query.CommandText = "CALL insert_funcionario (@nome, @data_nascimento, @sexo, @rg, @cpf, @telefone, @email, @ctps, @funcao, @cep, @rua, @numero, @bairro, @cidade, @estado, 'Funcionário'); SELECT LAST_INSERT_ID();";
+
+                query.Parameters.AddWithValue("@nome", item.Nome);
+                query.Parameters.AddWithValue("@data_nascimento", item.DataNascimento.ToString("yyyy-MM-dd HH:mm:ss"));
+                query.Parameters.AddWithValue("@sexo", item.Sexo);
+                query.Parameters.AddWithValue("@rg", item.Rg);
+                query.Parameters.AddWithValue("@cpf", item.Cpf);
+                query.Parameters.AddWithValue("@telefone", item.Telefone);
+                query.Parameters.AddWithValue("@email", item.Email);
+                query.Parameters.AddWithValue("@ctps", item.Ctps);
+                query.Parameters.AddWithValue("@funcao", item.Funcao);
+                query.Parameters.AddWithValue("@cep", item.Endereco.CEP);
+                query.Parameters.AddWithValue("@rua", item.Endereco.Rua);
+                query.Parameters.AddWithValue("@numero", item.Endereco.Numero);
+                query.Parameters.AddWithValue("@bairro", item.Endereco.Bairro);
+                query.Parameters.AddWithValue("@cidade", item.Endereco.Cidade);
+                query.Parameters.AddWithValue("@estado", item.Endereco.Estado);
+                query.Parameters.AddWithValue("Funcionário", item.Endereco.Tipo);
+
+
+                var id = Convert.ToInt32(query.ExecuteScalar());
+                return id;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
     }
 }
