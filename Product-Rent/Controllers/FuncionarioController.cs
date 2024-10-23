3fa45e13615d6d48b494eb0db4ee71311497c201
Thiciane Frata <100950692+Thiciane@users.noreply.github.com>
2024-10-23 00:13:55 -0300

    🐛 fix: Arrumando indentação que tava errada (rsrs).

--- Product-Rent/Controllers/FuncionarioController.cs
+++ Product-Rent/Controllers/FuncionarioController.cs
@@ -9,55 +9,20 @@ namespace Product_Rent.Controllers
     [Route("Api/[Controller]")]
     public class FuncionarioController : ControllerBase
     {
-        [HttpGet]
-        public IActionResult Get()
-        {
-            List<Funcionario> funcionarios = new FuncionarioDAO().GetAll();
-            return Ok(funcionarios);
-        }
-
-        [HttpGet("{id}")]
-        public IActionResult GetById(int id)
-        {
-            Funcionario funcionario = new FuncionarioDAO().GetById(id);
-            return Ok(funcionario);
-        }
-
-[HttpPost]
-public IActionResult Create([FromBody] FuncionarioDTO item)
-{
-    //if (item.Cpf != "")
-    //{
-    //    if (ValidarCPF.ValidaCPF(item.Cpf) == false)
-    //    {
-    //        return BadRequest("CPF inválido.");
-    //    }
-    //}
-    var funcionario = new Funcionario();
-
-    funcionario.Nome = item.Nome;
-    funcionario.Cpf = item.Cpf;
-    funcionario.Rg = item.Rg;
-    funcionario.Telefone = item.Rg;
-    funcionario.Email = item.Email;
-    funcionario.DataNascimento = item.DataNascimento;
-    funcionario.Sexo = item.Sexo;
-    funcionario.Ctps = item.Ctps;
-    funcionario.Funcao = item.Funcao;
-    funcionario.Endereco = item.Endereco;
-    funcionario.Status = true;
-
-    try
+    [HttpGet]
+    public IActionResult Get()
     {
-        var dao = new FuncionarioDAO();
-        funcionario.Id = dao.Insert(funcionario);
+        List<Funcionario> funcionarios = new FuncionarioDAO().GetAll();
+        return Ok(funcionarios);
     }
-    catch (Exception ex)
+
+    [HttpGet("{id}")]
+    public IActionResult GetById(int id)
     {
-        return BadRequest(ex.Message);
+        Funcionario funcionario = new FuncionarioDAO().GetById(id);
+        return Ok(funcionario);
     }
-    return Created("", funcionario);
-    }
+
     [HttpPost]
     public IActionResult Create([FromBody] FuncionarioDTO item)
     {
@@ -91,44 +56,79 @@ public IActionResult Create([FromBody] FuncionarioDTO item)
         {
             return BadRequest(ex.Message);
         }
-        return Created("", funcionario);
+        return Created("", funcionario);
+        }
+    [HttpPost]
+    public IActionResult Create([FromBody] FuncionarioDTO item)
+    {
+        //if (item.Cpf != "")
+        //{
+        //    if (ValidarCPF.ValidaCPF(item.Cpf) == false)
+        //    {
+        //        return BadRequest("CPF inválido.");
+        //    }
+        //}
+        var funcionario = new Funcionario();
+
+        funcionario.Nome = item.Nome;
+        funcionario.Cpf = item.Cpf;
+        funcionario.Rg = item.Rg;
+        funcionario.Telefone = item.Rg;
+        funcionario.Email = item.Email;
+        funcionario.DataNascimento = item.DataNascimento;
+        funcionario.Sexo = item.Sexo;
+        funcionario.Ctps = item.Ctps;
+        funcionario.Funcao = item.Funcao;
+        funcionario.Endereco = item.Endereco;
+        funcionario.Status = true;
+
+        try
+        {
+            var dao = new FuncionarioDAO();
+            funcionario.Id = dao.Insert(funcionario);
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+        return Created("", funcionario);
     }
 
-        [HttpPut("{Id}")]
-        public IActionResult Update(int id, [FromBody] FuncionarioDTO item)
+    [HttpPut("{Id}")]
+    public IActionResult Update(int id, [FromBody] FuncionarioDTO item)
+    {
+        if (item == null)
         {
-            if (item == null)
+            return NotFound();
+        }
+        
+        if (item.Cpf != "")
+        {
+            if (ValidarCPF.ValidaCPF(item.Cpf) == false)
             {
-                return NotFound();
+                return BadRequest("CPF inválido.");
             }
-            
-            if (item.Cpf != "")
-            {
-                if (ValidarCPF.ValidaCPF(item.Cpf) == false)
-                {
-                    return BadRequest("CPF inválido.");
-                }
-            }
-
-            var funcionario = FuncionarioOperacoes.Update(id, item);
-            if (funcionario == null)
-            {
-                return NotFound();
-            }
-
-            return Ok(funcionario);
         }
 
-        [HttpDelete("{Id}")]
-        public IActionResult Delete(int id)
+        var funcionario = FuncionarioOperacoes.Update(id, item);
+        if (funcionario == null)
         {
-            var verificar = FuncionarioOperacoes.Delete(id);
-            if (!verificar)
-            {
-                return NotFound();
-            }
-
-            return NoContent();
+            return NotFound();
         }
+
+        return Ok(funcionario);
+    }
+
+    [HttpDelete("{Id}")]
+    public IActionResult Delete(int id)
+    {
+        var verificar = FuncionarioOperacoes.Delete(id);
+        if (!verificar)
+        {
+            return NotFound();
+        }
+
+        return NoContent();
+    }
     }
 }
