4935891218ae71ba2b6252e143eb17acfad90e1e
Thiciane Frata <100950692+Thiciane@users.noreply.github.com>
2024-10-22 23:50:56 -0300

    🐛 fix: Arrumando as tabelas que estavam erradas

--- Product-Rent/DataBase/Elegance_Rent_DataBase.sql
+++ Product-Rent/DataBase/Elegance_Rent_DataBase.sql
@@ -1,176 +1,168 @@
 create database Elegance_Rent;
 use Elegance_Rent;
 
-create table Endereco (
-id_end int primary key not null auto_increment,
-cep_end varchar(200),
-cidade_end varchar (200),
-estado_end varchar (200),
-rua_end varchar (200),
-bairro_end varchar (200),
-numero_end int
+CREATE TABLE Cliente (
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(100),
+    data_nascimento DATE,
+    sexo VARCHAR(10),
+    rg VARCHAR(12),
+    cpf VARCHAR(14),
+    cnpj VARCHAR(18),
+    telefone VARCHAR(15),
+    email VARCHAR(100)
 );
 
-create table Cliente (
-id_cli int primary key not null auto_increment,
-nome_cli varchar (1000),
-data_nascimento_cli date,
-sexo_cli varchar (100),
-rg_cli varchar (18),
-cnpj_cli varchar (40),
-cpf_cli varchar (40),
-telefone_cli varchar(16),
-email_cli varchar (300),
-
-id_end_fk int,
-foreign key (id_end_fk) references Endereco (id_end)
+CREATE TABLE Funcionario (
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(100),
+    data_nascimento DATE,
+    sexo VARCHAR(10),
+    rg VARCHAR(12),
+    cpf VARCHAR(14),
+    ctps VARCHAR(15),
+    funcao VARCHAR(50),
+    telefone VARCHAR(15),
+    email VARCHAR(100),
+    ativo bool
 );
 
-create table Funcionario (
-id_fun int primary key not null auto_increment,
-nome_fun varchar (1000),
-cpf_fun varchar (40),
-rg_fun varchar (18),
-telefone_fun varchar(16),
-email_fun varchar (300),
-data_nascimento_fun date,
-sexo_fun varchar (100),
-ctps_fun varchar (150),
-funcao_fun varchar (200),
-
-id_end_fk int,
-foreign key (id_end_fk) references Endereco (id_end)
+CREATE TABLE Fornecedor (
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    razao_social VARCHAR(100),
+    nome_fantasia VARCHAR(100),
+    cnpj VARCHAR(18),
+    inscricao_estadual VARCHAR(20),
+    inscricao_municipal VARCHAR(20),
+    responsavel VARCHAR(100),
+    contato_1 VARCHAR(15),
+    contato_2 VARCHAR(15),
+    contato_3 VARCHAR(15),
+    email_1 VARCHAR(100),
+    email_2 VARCHAR(100)
 );
 
-create table Aluguel (
-id_alu int primary key not null auto_increment,
-data_retirada_alu date,
-data_devolucao_alu date,
-valor_total_alu double,
-
-id_fun_fk int,
-id_cli_fk int,
-foreign key (id_fun_fk) references Funcionario (id_fun),
-foreign key (id_cli_fk) references Cliente (id_cli)
+CREATE TABLE Endereco (
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    cep VARCHAR(10),
+    rua VARCHAR(100),
+    numero INT,
+    bairro VARCHAR(50),
+    cidade VARCHAR(50),
+    estado VARCHAR(2),
+    tipo_user ENUM('Cliente', 'Funcionário', 'Fornecedor'),
+    id_cli_fk INT UNIQUE,
+    id_fun_fk INT UNIQUE,
+    id_for_fk INT UNIQUE,
+    FOREIGN KEY (id_cli_fk) REFERENCES Cliente (id),
+    FOREIGN KEY (id_fun_fk) REFERENCES Funcionario (id),
+    FOREIGN KEY (id_for_fk) REFERENCES Fornecedor (id)
 );
 
-create table Fornecedor (
-id_for int primary key not null auto_increment,
-cnpj_for varchar (200),
-razao_social_for varchar (200),
-nome_fantasia_for varchar (200),
-inscricao_estadual_for varchar (200),
-inscricao_municipal_for varchar (200),
-responsavel_for varchar (200),
-contato_1_for varchar (200),
-contato_2_for varchar (200),
-contato_3_for varchar (200),
-email_1_for varchar (200),
-email_2_for varchar (200),
+#insert into endereco (id, cep, rua, numero, bairro, cidade, estado, tipo_user) values (null, 'cep', 'rua', 520, 'bairro', 'cidade', 'es', 'cliente');
 
-id_end_fk int,
-foreign key (id_end_fk) references Endereco (id_end)
+CREATE TABLE Aluguel (
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    data_retirada DATE,
+    data_devolucao DATE,
+    valor_total DOUBLE,
+    id_fun_fk INT,
+    id_cli_fk INT,
+    FOREIGN KEY (id_fun_fk) REFERENCES Funcionario (id),
+    FOREIGN KEY (id_cli_fk) REFERENCES Cliente (id)
 );
 
-create table Produto (
-id_prod int primary key not null auto_increment,
-nome_prod varchar (300),
-marca_prod varchar (300),
-tamanho_prod varchar (300),
-cor_prod varchar (300),
-valor_aluguel_prod varchar (300),
-descricao_prod varchar (500),
-
-id_for_fk int,
-foreign key (id_for_fk) references Fornecedor (id_for)
+CREATE TABLE Produto (
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(100),
+    marca VARCHAR(100),
+    tamanho VARCHAR(10),
+    cor VARCHAR(30),
+    valor_aluguel DECIMAL(10, 2),
+    descricao VARCHAR(500),
+    id_for_fk INT,
+    FOREIGN KEY (id_for_fk) REFERENCES Fornecedor (id)
 );
 
-create table Compra (
-id_comp int primary key not null auto_increment,
-valor_total_comp double,
-data_comp date,
-forma_de_pagamento varchar (300),
-
-
-id_fun_fk int,
-id_for_fk int,
-foreign key (id_fun_fk) references Funcionario (id_fun),
-foreign key (id_for_fk) references Fornecedor (id_for)
+CREATE TABLE Compra (
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    data DATE,
+    valor_total DOUBLE,
+    forma_de_pagamento VARCHAR(50),
+    id_fun_fk INT,
+    id_for_fk INT,
+    FOREIGN KEY (id_fun_fk) REFERENCES Funcionario (id),
+    FOREIGN KEY (id_for_fk) REFERENCES Fornecedor (id)
 );
 
-create table Compra_Produto (
-id_cprod int primary key not null auto_increment,
-valor_total_cprod double,
-valor_unitario_cprod double,
-quantidade_cprod int,
-
-id_prod_fk int,
-id_comp_fk int,
-foreign key (id_prod_fk) references Produto (id_prod),
-foreign key (id_comp_fk) references Compra (id_comp)
+CREATE TABLE Compra_Produto (
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    valor_total DOUBLE,
+    valor_unitario DOUBLE,
+    quantidade INT,
+    id_prod_fk INT,
+    id_comp_fk INT,
+    FOREIGN KEY (id_prod_fk) REFERENCES Produto (id),
+    FOREIGN KEY (id_comp_fk) REFERENCES Compra (id)
 );
 
-create table Aluguel_Produto (
-id_aprod int primary key not null auto_increment,
-valor_total_aprod double,
-valor_unitario_aprod double,
-quantidade_aprod int,
-
-id_prod_fk int,
-id_alu_fk int,
-foreign key (id_prod_fk) references Produto (id_prod),
-foreign key (id_alu_fk) references Aluguel (id_alu)
+CREATE TABLE Aluguel_Produto (
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    valor_total DOUBLE,
+    valor_unitario DOUBLE,
+    quantidade INT,
+    id_prod_fk INT,
+    id_alu_fk INT,
+    FOREIGN KEY (id_prod_fk) REFERENCES Produto (id),
+    FOREIGN KEY (id_alu_fk) REFERENCES Aluguel (id)
 );
 
-create table Caixa (
-id_cai int primary key not null auto_increment,
-numero_cai int,
-data_cai date,
-saldo_inicial_cai float,
-saldo_final_cai float,
-total_recebimentos_cai float,
-total_retiradas_cai float,
-
-id_fun_fk int,
-foreign key (id_fun_fk) references Funcionario (id_fun)
+CREATE TABLE Caixa (
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    numero INT,
+    data DATE,
+    saldo_inicial DECIMAL(10, 2),
+    saldo_final DECIMAL(10, 2),
+    total_recebimentos DECIMAL(10, 2),
+    total_retiradas DECIMAL(10, 2),
+    id_fun_fk INT,
+    FOREIGN KEY (id_fun_fk) REFERENCES Funcionario (id)
 );
 
-create table Recebimento (
-id_rec int primary key not null auto_increment,
-status_rec varchar (100),
-valor_rec double,
-parcela_rec int,
-data_rec date,
-forma_rec varchar (100),
-vencimento_rec date,
-
-id_cai_fk int,
-id_alu_fk int,
-foreign key (id_cai_fk) references Caixa (id_cai),
-foreign key (id_alu_fk) references Aluguel (id_alu)
+CREATE TABLE Recebimento (
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    status VARCHAR(20),
+    valor DOUBLE,
+    parcela INT,
+    data DATE,
+    forma VARCHAR(50),
+    vencimento DATE,
+    id_cai_fk INT,
+    id_alu_fk INT,
+    FOREIGN KEY (id_cai_fk) REFERENCES Caixa (id),
+    FOREIGN KEY (id_alu_fk) REFERENCES Aluguel (id)
 );
 
-create table Despesa (
-id_des int primary key not null auto_increment,
-nome_des varchar (300),
-data_des datetime,
-vencimento_des date,
-parcelamento_des int,
-descricao_des varchar (300)
+CREATE TABLE Despesa (
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(100),
+    data DATETIME,
+    vencimento DATE,
+    parcelamento INT,
+    descricao VARCHAR(300)
 );
 
-create table Pagamento (
-id_pag int primary key not null auto_increment,
-status_pag bool,
-valor_pag double,
-parcela_pag int,
-data_pag date,
-forma_pag varchar (100),
-
-id_cai_fk int,
-id_des_fk int,
-id_comp_fk int,
-foreign key (id_cai_fk) references Caixa (id_cai),
-foreign key (id_des_fk) references Despesa (id_des),
-foreign key (id_comp_fk) references Compra (id_comp)
+CREATE TABLE Pagamento (
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    status BOOL,
+    valor DOUBLE,
+    parcela INT,
+    data DATE,
+    forma VARCHAR(50),
+    id_cai_fk INT,
+    id_des_fk INT,
+    id_comp_fk INT,
+    FOREIGN KEY (id_cai_fk) REFERENCES Caixa (id),
+    FOREIGN KEY (id_des_fk) REFERENCES Despesa (id),
+    FOREIGN KEY (id_comp_fk) REFERENCES Compra (id)
 );
