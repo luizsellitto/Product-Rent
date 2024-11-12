28404c9628ad94a26e669a4bfaacfbf64aa00a5b
Luizsellitto <luizsellitto2609@gmail.com>
2024-11-11 20:03:29 -0400

    :bug: bug fixed: ativo - status
    Fornecedor e Funcionario

--- Product-Rent/Models/FuncionarioDAO.cs
+++ Product-Rent/Models/FuncionarioDAO.cs
@@ -82,7 +82,7 @@ namespace Product_Rent.Models
                             Cidade = reader.GetString("cidade"),
                             Estado = reader.GetString("estado"),
                         },
-                        Status = reader.GetBoolean("ativo")
+                        Status = reader.GetBoolean("status")
                     });
 
                 }
@@ -131,7 +131,7 @@ namespace Product_Rent.Models
                             Cidade = reader.GetString("cidade"),
                             Estado = reader.GetString("estado")
                         },
-                        Status = reader.GetBoolean("ativo")
+                        Status = reader.GetBoolean("status")
                     };
                 }
                 return funcionario;
